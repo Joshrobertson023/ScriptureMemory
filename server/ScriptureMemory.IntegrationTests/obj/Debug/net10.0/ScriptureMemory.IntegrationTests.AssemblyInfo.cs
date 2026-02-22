fb20e0f038cd1982cc2754dfd4e129089fe067f2
Josh Robertson <therealjoshrobertson@gmail.com>
2026-02-22 17:56:25 -0600

    Refactored root layout

--- server/ScriptureMemory.IntegrationTests/obj/Debug/net10.0/ScriptureMemory.IntegrationTests.AssemblyInfo.cs
+++ server/ScriptureMemory.IntegrationTests/obj/Debug/net10.0/ScriptureMemory.IntegrationTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScriptureMemory.IntegrationTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d566627caa91f3d5160e33448d5b636737913334")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+444bdffb80158a76a30bd6d07328275700d541c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScriptureMemory.IntegrationTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScriptureMemory.IntegrationTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
