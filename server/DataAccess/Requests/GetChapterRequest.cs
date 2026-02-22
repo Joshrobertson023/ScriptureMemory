d566627caa91f3d5160e33448d5b636737913334
Josh Robertson <therealjoshrobertson@gmail.com>
2026-02-21 21:23:18 -0600

    Refactored verse endpoint

---
+++ server/DataAccess/Requests/GetChapterRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Requests;
+
+public sealed class GetChapterRequest
+{
+    [Required] public string Book { get; set; } = string.Empty;
+    [Required] public int Chapter { get; set; }
+}
--- server/ScriptureMemory.Server/Endpoints/VerseEndpoint.cs
+++ server/ScriptureMemory.Server/Endpoints/VerseEndpoint.cs
@@ -1,149 +1,74 @@
-//using DataAccess.DataInterfaces;
-//using DataAccess.Models;
-//using Microsoft.AspNetCore.Mvc;
-//using System.Diagnostics;
-//using System.Security.Cryptography.Xml;
-//using System.Text.Json;
-//using ScriptureMemoryLibrary;
+using DataAccess.DataInterfaces;
+using DataAccess.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Security.Cryptography.Xml;
+using System.Text.Json;
+using ScriptureMemoryLibrary;
+using DataAccess.Requests;
 
-//namespace VerseAppNew.Server.Endpoints;
+namespace VerseAppNew.Server.Endpoints;
 
-//public static class VerseEndpoint
-//{
-//    public static void ConfigureVerseEndpoints(this WebApplication app)
-//    {
-//        app.MapGet("/verses/{reference}", GetVerse);
-//        app.MapGet("/verses/chapter/{book}/{chapter}", GetChapterVerses);
-//        app.MapPost("/verses", InsertVerse);
-//        app.MapPut("/verses/saved/{reference}", UpdateUsersSavedVerse);
-//        app.MapPut("/verses/memorized/{reference}", UpdateUsersMemorizedVerse);
-//        app.MapGet("/verses/search/{search}", GetVersesSearchResult);
-//        app.MapGet("/verses/top/saved/{top}", GetTopSavedVerses);
-//        app.MapGet("/verses/top/memorized/{top}", GetTopMemorizedVerses);
-//    }
+public static class VerseEndpoint
+{
+    public static void ConfigureVerseEndpoints(this WebApplication app)
+    {
+        app.MapGet("/verses", async (
+            [FromBody] string reference, 
+            [FromServices] IVerseData data) =>
+        {
+            var results = await data.GetVerse(reference);
+            if (results == null)
+                return Results.NotFound();
+            return Results.Ok(results);
+        });
 
-//    public static async Task<IResult> GetChapterVerses(
-//        string book,
-//        int chapter,
-//        [FromServices] IVerseData data)
-//    {
-//        try
-//        {
-//            var results = await data.GetChapterVerses(book, chapter);
-//            return Results.Ok(results);
-//        }
-//        catch (Exception ex)
-//        {
-//            return Results.Problem(ex.Message);
-//        }
-//    }
+        app.MapGet("/verses/chapter", async (
+            [FromBody] GetChapterRequest request,
+            [FromServices] IVerseData data) =>
+        {
+            var results = await data.GetChapterVerses(request.Book, request.Chapter);
+            return Results.Ok(results);
+        });
 
-//    public static async Task<IResult> GetVersesSearchResult(
-//        string search,
-//        [FromServices] IVerseData data)
-//    {
-//        try
-//        {
-//            var results = await data.GetVerseSearchResults(search);
-//            return Results.Ok(results);
-//        }
-//        catch (Exception ex)
-//        {
-//            return Results.Problem(ex.Message);
-//        }
-//    }
+        app.MapPut("/verses/saved/{reference}", async (
+            string reference, 
+            [FromServices] IVerseData data) =>
+        {
+            await data.UpdateUsersSavedVerse(reference);
+            return Results.Ok();
+        });
 
-//    public static async Task<IResult> GetVerse(
-//        string reference, 
-//        [FromServices] IVerseData data)
-//    {
-//        try
-//        {
-//            var results = await data.GetVerse(reference);
-//            if (results == null)
-//                return Results.NotFound();
-//            return Results.Ok(results);
-//        }
-//        catch (Exception ex)
-//        {
-//            return Results.Problem(ex.Message);
-//        }
-//    }
+        app.MapPut("/verses/memorized/{reference}", async (
+            string reference, 
+            [FromServices] IVerseData data) =>
+        {
+            await data.UpdateUsersMemorizedVerse(reference);
+            return Results.Ok();
+        });
 
-//    public static async Task<IResult> InsertVerse(
-//        [FromBody] Verse verse, 
-//        [FromServices] IVerseData data)
-//    {
-//        try
-//        {
-//            await data.InsertVerse(verse);
-//            return Results.Ok();
-//        }
-//        catch (Exception ex)
-//        {
-//            return Results.Problem(ex.Message);
-//        }
-//    }
+        //app.MapGet("/verses/search/{search}", async (string search, [FromServices] IVerseData data) =>
+        //{
+        //    var results = await data.GetVerseSearchResults(search);
+        //    return Results.Ok(results);
+        //});
 
-//    public static async Task<IResult> UpdateUsersSavedVerse(
-//        string reference, 
-//        [FromServices] IVerseData data)
-//    {
-//        try
-//        {
-//            await data.UpdateUsersSavedVerse(reference);
-//            return Results.Ok();
-//        }
-//        catch (Exception ex)
-//        {
-//            return Results.Problem(ex.Message);
-//        }
-//    }
+        app.MapGet("/verses/top/saved/{top}", async (
+            int top, 
+            [FromServices] IVerseData data) =>
+        {
+            if (top == 0) top = 30;
+            var results = await data.GetTopSavedVerses(top);
+            return Results.Ok(results);
+        });
 
-//    public static async Task<IResult> UpdateUsersMemorizedVerse(
-//        string reference, 
-//        [FromServices] IVerseData data)
-//    {
-//        try
-//        {
-//            await data.UpdateUsersMemorizedVerse(reference);
-//            return Results.Ok();
-//        }
-//        catch (Exception ex)
-//        {
-//            return Results.Problem(ex.Message);
-//        }
-//    }
-
-//    public static async Task<IResult> GetTopSavedVerses(
-//        int top,
-//        [FromServices] IVerseData data)
-//    {
-//        try
-//        {
-//            if (top == 0) top = 30;
-//            var results = await data.GetTopSavedVerses(top);
-//            return Results.Ok(results);
-//        }
-//        catch (Exception ex)
-//        {
-//            return Results.Problem(ex.Message);
-//        }
-//    }
-
-//    public static async Task<IResult> GetTopMemorizedVerses(
-//        int top,
-//        [FromServices] IVerseData data)
-//    {
-//        try
-//        {
-//            if (top == 0) top = 30;
-//            var results = await data.GetTopMemorizedVerses(top);
-//            return Results.Ok(results);
-//        }
-//        catch (Exception ex)
-//        {
-//            return Results.Problem(ex.Message);
-//        }
-//    }
-//}
+        app.MapGet("/verses/top/memorized/{top}", async (
+            int top, 
+            [FromServices] IVerseData data) =>
+        {
+            if (top == 0) top = 30;
+            var results = await data.GetTopMemorizedVerses(top);
+            return Results.Ok(results);
+        });
+    }
+}
